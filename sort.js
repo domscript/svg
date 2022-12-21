22e1aa8160ffd9cdb18f0fb7048c4ebd90a0ba88
Dominic <89986367+domscript@users.noreply.github.com>
2022-12-22 03:29:25 +0700

    Change for graph

--- sort.js
+++ sort.js
@@ -1,9 +1,5 @@
-prepareObj(numbers) {
-  const numObj = {}
-  for (const [i,num] of numbers.entries()) {
-    numObj[i] = num
-  }
-  return numObj;
+function prepareObj(numbers) {
+  return numbers.map((el,i)=>({[i]:el}));
 }
 
   function mergeSort(numbers) {
@@ -31,11 +27,10 @@ function merge(left, right) {
   // While there are elements in both arrays, compare the elements
   // and add the smaller one to the result array
   while (left.length > 0 && right.length > 0) {
-    if (left[0] <= right[0]) {
-     // console.l
+    if (Object.values(left[0]).at(0) <= Object.values(right[0]).at(0)) {
       result.push(left.shift());
     } else {
-      console.log(`Swapping ${left[0]} and ${right[0]}, left: ${left}, right: ${right}`);
+      console.log(`Swapping ${Object.entries(left[0]).at(0)} and ${Object.entries(right[0]).at(0)}`);
       result.push(right.shift());
     }
   }
@@ -53,6 +48,4 @@ function merge(left, right) {
   return result;
 }
 
-mergeSort([4,6,8,5,9,2,1,5,3,7]);
-
-console.log(prepareObj([4,6,8,5,9,2,1,5,3,7])
+mergeSort(prepareObj([4,6,8,5,9,2,1,5,3,7]));
